fd14903fe8e4700bd33c18357e48ec1f82e1c288
Surgeondentist <jgga02@gmail.com>
2024-09-09 17:00:47 -0500

    Boton Crear Usuario
    boton crear usuario guarda la informacion de las entradas de texto a traves del metodo POST para luego utilizarlo a traves de la funcion insertar y agregar un registro a nuestra base de datos.

--- Controladores/ControladorUsuarios.php
+++ Controladores/ControladorUsuarios.php
@@ -10,6 +10,30 @@ class ControladorUsuarios extends ControladorBase {
     }
 
     public function crear(){
+        
+        if (isset($_POST['btn_registrar'])) {
+            echo "Formulario Enviado";
+            $nombre = $_POST['Nombre'];
+            $email = $_POST['email'];
+            $contrasena = $_POST['contrasena'];
+            $usuarioGuardado = ConexionDB::get()->insertar("usuarios",[
+                'nombre' => $nombre,
+                'email' => $email,
+                'contrasena' => $contrasena
+            ]
+            );
+            if ($usuarioGuardado) {
+                # Redireccionar a inicio
+                header('Location: http://localhost/proyectolibreria/?controlador=usuarios&accion=listado');
+                exit();
+            } else {
+                throw new Exception("No se pudo guardar el usuario");
+                exit();
+                
+            }
+            
+        }
+
         $this->mostrarVista("usuarios","crear");
     }
     public function actualizar(){
--- Vistas/Usuarios/crear.php
+++ Vistas/Usuarios/crear.php
@@ -1,26 +1,26 @@
 
 <h1>Crear nuevo usuario</h1>
 <hr>
-<form >
+<form method="POST" action="./?controlador=usuarios&accion=crear">
 
 <div class="mb-3">
     <label for="Nombre" class="form-label">Nombre del Usuario</label>
-    <input class="form-control" id="Nombre">
+    <input class="form-control" id="Nombre" name="Nombre">
 </div>
 
 <div class="mb-3">
     <label for="email" class="form-label" >E-mail del usuario</label>
-    <input class="form-control" id="email"/>
+    <input class="form-control" id="email" name="email"/>
 </div>
 
 <div class="mb-3">
     <label for="contrasena" class="form-label" >Contrasena para el usuario</label>
-    <input class="form-control" id="contrasena"/>
+    <input class="form-control" id="contrasena" name="contrasena"/>
 </div>
     
 <div class="mb-3">
     <a class="btn btn-secondary" href="./?controlador=usuarios&accion=listado">Cancelar</a>
-    <button  class="btn btn-success"  type="submit">Guardar</button>
+    <button  class="btn btn-success"  type="submit"  name="btn_registrar">Guardar</button>
 </div>
 
 </form>
