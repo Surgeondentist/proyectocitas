43ff8cfcdb3fedc99ed8d11ca41ce1ce7d15e5a7
Surgeondentist <jgga02@gmail.com>
2024-09-23 13:14:15 -0500

    Leer y Crear
    De realiza creacion de tabla Citas en phpadmin, y se desarrolla el controladorCitas junto con la Vista correspondiente que permite ingresar los datos para una nueva cita.

--- Controladores/ControladorCitas.php
+++ Controladores/ControladorCitas.php
@@ -3,11 +3,36 @@
 class ControladorCitas extends ControladorBase {
     
     public function listado(){
-        $this->mostrarVista("Citas", "listado");
+        $query = "SELECT * FROM citas";
+        $citas = ConexionDB::get()->query($query);
+        $this->mostrarVista("citas","listado", ['datos' => $citas ]);
     }
 
     public function crear(){
-        $this->mostrarVista("Citas", "crear");
+        
+        if (isset($_POST['btn_registrar'])) {
+            echo "Formulario Enviado";
+            $fecha = $_POST['fecha'];
+            $paciente = $_POST['paciente'];
+            $citaGuardado = ConexionDB::get()->insertar("citas",[
+                'fecha' => $fecha,
+                'paciente' => $paciente,
+
+            ]);
+            if ($citaGuardado) {
+                # Redireccionar a inicio
+                #header('Location: http://localhost/proyectolibreria/?controlador=usuarios&accion=listado');
+                #exit();
+                $this->app->redireccionar("citas", "listado");
+            } else {
+                throw new Exception("No se pudo guardar el cita");
+                exit();
+                
+            }
+            
+        }
+
+        $this->mostrarVista("citas","crear");
     }
 
 }
--- Vistas/Citas/crear.php
+++ Vistas/Citas/crear.php
@@ -1,2 +1,23 @@
-<h1>Crear nueva cita</h1>
-<p>Aqui mostraremos el formulario</p>
+<h1>Crear nueva Cita</h1>
+<hr>
+<form method="POST" action="./?controlador=citas&accion=crear">
+
+<div class="mb-3">
+    <label for="fecha" class="form-label">Fecha para la cita</label>
+    <input class="form-control" id="fecha" name="fecha">
+</div>
+
+<div class="mb-3">
+    <label for="paciente" class="form-label" >id del paciente</label>
+    <input class="form-control" id="paciente" name="paciente"/>
+</div>
+    
+<div class="mb-3">
+    <a class="btn btn-secondary" href="./?controlador=citas&accion=listado">Cancelar</a>
+    <button  
+    class="btn btn-success"  
+    type="submit"  
+    name="btn_registrar">Guardar</button>
+</div>
+
+</form>
